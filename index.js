074865ef0432692e95c92b69c892acc4adfcc51e
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2023-07-24 15:29:36 +0700

    Refactoring, improvements, and more magic! 🪄

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { watch } = require('chokidar');
-const { writeFileSync, removeSync, readFileSync } = require('fs-extra');
+const { writeFileSync, removeSync, readFileSync, ensureFileSync, ensureDirSync } = require('fs-extra');
 const { spawn, spawnSync } = require('child_process');
 const { join } = require('path');
 const glob = require('glob');
@@ -7,27 +7,65 @@ const glob = require('glob');
 const watchScript = process.argv.includes('--watch');
 const clean = process.argv.includes('--clean');
 
-if (clean) {
-  removeSync(join(process.cwd(), 'dist'));
+const cwd = process.cwd();
+const esm = require(join(cwd, 'tsconfig.json'));
+const cjs = require(join(cwd, 'tsconfig-cjs.json'));
+
+const esmOutDir = join(cwd, esm.compilerOptions?.outDir || 'dist/esm');
+const cjsOutDir = join(cwd, cjs.compilerOptions?.outDir || 'dist/cjs');
+
+if (esmOutDir) {
+  if (clean) {
+    removeSync(esmOutDir);
+  }
+
+  ensureDirSync(esmOutDir);
 }
 
-if (watchScript) {
-  spawn('tsc', ['-w', '-p', './tsconfig.json'], { stdio: 'inherit', shell: true });
-  spawn('tsc', ['-w', '-p', './tsconfig-cjs.json'], { stdio: 'inherit', shell: true });
+if (cjsOutDir) {
+  if (clean) {
+    removeSync(cjsOutDir);
+  }
 
-  watch('./dist').on('all', (event, file) => {
-    if (event === 'add' && file.includes('cjs') && file.endsWith('.js')) {
-      rename(join(process.cwd(), file), 'cjs');
+  ensureDirSync(cjsOutDir);
+}
+
+const changeList = [];
+
+if (watchScript) {
+  watch(cjsOutDir).on('all', (event, file) => {
+    if (event === 'add' && file.endsWith('.js')) {
+      rename(file, 'cjs');
     }
   });
+
+  watch(esmOutDir).on('all', (event, file) => {
+    if (file.endsWith('.cjs')) {
+      if (changeList.includes(file)) {
+        changeList.splice(changeList.indexOf(file), 1);
+      } else {
+        changeList.push(file);
+        cleanup(file);
+      }
+    }
+  });
+
+  spawn('tsc', ['-w', '-p', './tsconfig.json'], { stdio: 'inherit', shell: true });
+  spawn('tsc', ['-w', '-p', './tsconfig-cjs.json'], { stdio: 'inherit', shell: true });
 } else {
   spawnSync('tsc', ['-p', './tsconfig.json'], { stdio: 'inherit', shell: true });
   spawnSync('tsc', ['-p', './tsconfig-cjs.json'], { stdio: 'inherit', shell: true });
 
-  const files = glob.sync('./dist/cjs/**/*.js');
+  const files = glob.sync(`${cjsOutDir}/**/*.js`);
 
   for (const file of files) {
     rename(file, 'cjs');
+  }
+
+  const esFiles = glob.sync(`${esmOutDir}/**/*.cjs`);
+
+  for (const file of esFiles) {
+    cleanup(file);
   }
 }
 
@@ -38,4 +76,10 @@ function rename(file, ext = 'cjs') {
 
   writeFileSync(path, text, 'utf-8');
   removeSync(file);
+}
+
+function cleanup(file) {
+  const content = readFileSync(file, 'utf-8');
+  const text = content.replaceAll('export {};', '');
+  writeFileSync(file, text, 'utf-8');
 }
